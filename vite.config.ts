25b8470459a72711ba63e7464f4cad29766f5a94
Kleverson Silva Jara <kleverson.silva@unemat.br>
2025-06-05 15:28:08 -0400

    melhorias gerais

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import postcss from 'postcss';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+  },
+  css: {
+    postcss: './postcss.config.js'
   },
   plugins: [
     react(),
